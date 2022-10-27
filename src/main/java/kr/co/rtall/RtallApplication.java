e75f0e6c01b13af81dc9fb5c708b49a372342f21
UBIVELOX <UBIVELOX@UB-D40-160603>
2022-10-27 10:17:48 +0900

    sdf

--- src/main/java/kr/co/rtall/RtallApplication.java
+++ src/main/java/kr/co/rtall/RtallApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RtallApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RtallApplication.class, args);
+		SpringApplication.run(
+				RtallApplication.class, args
+				);
 	}
 
 }
