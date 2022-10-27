4ee23afc6256d4e973966cbfd137664d0912469b
UBIVELOX <UBIVELOX@UB-D40-160603>
2022-10-27 10:11:36 +0900

    init commit

---
+++ src/main/java/kr/co/rtall/RtallApplication.java
@@ -0,0 +1,13 @@
+package kr.co.rtall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtallApplication.class, args);
+	}
+
+}
